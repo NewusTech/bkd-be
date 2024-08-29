93e228c40468b82a8b99a5254bfb06d7dd54cc8f
My Name <lulu.agustin105319@students.unila.ac.id>
2024-08-29 16:37:49 +0700

    add

--- routes/api.route.js
+++ routes/api.route.js
@@ -2,10 +2,14 @@ const userRoute = require('./user.route');
 const userinfoRoute = require('./userinfo.route');
 const roleRoute = require('./role.route');
 const permissionRoute = require('./permission.route');
+const bidangRoute = require('./bidang.route');
+const layananRoute = require('./layanan.route');
 
 module.exports = function (app, urlApi) {
     app.use(urlApi, userRoute);
     app.use(urlApi, userinfoRoute);
     app.use(urlApi, roleRoute);
     app.use(urlApi, permissionRoute);
+    app.use(urlApi, bidangRoute);
+    app.use(urlApi, layananRoute);
 }
