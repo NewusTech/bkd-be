425ee65c35792a150d6075e5199a9c926ecbe015
My Name <lulu.agustin105319@students.unila.ac.id>
2024-08-29 14:01:00 +0700

    create model user

---
+++ models/userinfo.js
@@ -0,0 +1,53 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User_info extends Model {
+    static associate(models) {
+      User_info.hasOne(models.User, {
+        foreignKey: 'userinfo_id',
+      });
+    }
+  }
+  User_info.init({
+    name: DataTypes.STRING,
+    nip: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+    nik: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+    slug: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+    telepon: DataTypes.STRING,
+    alamat: DataTypes.STRING,
+    agama: DataTypes.INTEGER,
+    tempat_lahir: DataTypes.STRING,
+    tgl_lahir: DataTypes.DATEONLY,
+    status_kawin: DataTypes.SMALLINT,
+    gender: DataTypes.SMALLINT,
+    pekerjaan: DataTypes.STRING,
+    goldar: DataTypes.SMALLINT,
+    pendidikan: DataTypes.SMALLINT,
+    pas_photo: DataTypes.STRING,
+    sk_cpns: DataTypes.STRING,
+    sk_pns: DataTypes.STRING,
+    akta_kelahiran: DataTypes.STRING,
+    file_ktp: DataTypes.STRING,
+    file_kk: DataTypes.STRING,
+    sk_konversi_nip: DataTypes.STRING,
+    kartu_bpjs: DataTypes.STRING,
+    kartu_npwp: DataTypes.STRING,
+    deletedAt: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'User_info',
+  });
+  return User_info;
+};
