5fecda5ceb2156f36fd924de0f9e3bc6bb45ff22
My Name <lulu.agustin105319@students.unila.ac.id>
2024-08-30 13:40:19 +0700

    migration & model layanan form num

---
+++ migrations/20240830024357-create-layanan-form-num.js
@@ -0,0 +1,65 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Layanan_form_nums', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userinfo_id: {
+        type: Sequelize.INTEGER
+      },
+      layanan_id: {
+        type: Sequelize.INTEGER
+      },
+      fileoutput: {
+        type: Sequelize.STRING
+      },
+      pesan: {
+        type: Sequelize.STRING
+      },
+      tgl_selesai: {
+        type: Sequelize.DATEONLY
+      },
+      status: {
+        type: Sequelize.SMALLINT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+
+    await queryInterface.addConstraint('Layanan_form_nums', {
+      fields: ['userinfo_id'],
+      type: 'foreign key',
+      name: 'custom_fkey_userinfo_id',
+      references: {
+        table: 'User_infos',
+        field: 'id'
+      }
+    });
+
+    await queryInterface.addConstraint('Layanan_form_nums', {
+      fields: ['layanan_id'],
+      type: 'foreign key',
+      name: 'custom_fkey_layanan_id',
+      references: {
+        table: 'Layanans',
+        field: 'id'
+      }
+    });
+  },
+
+  //untuk drop table ketika melakukan revert migrations
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Layanan_form_nums');
+  }
+};
---
+++ models/layananformnum.js
@@ -0,0 +1,38 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Layanan_form_num extends Model {
+    static associate(models) {
+      Layanan_form_num.belongsTo(models.Layanan, {
+        foreignKey: 'layanan_id',
+      });
+      Layanan_form_num.hasMany(models.Layananforminput, {
+        foreignKey: 'layananformnum_id',
+      });
+      Layanan_form_num.belongsTo(models.User_info, {
+        foreignKey: 'userinfo_id',
+      });
+      Layanan_form_num.belongsTo(models.User_info, {
+        foreignKey: 'updated_by',
+        as: 'Adminupdate'
+      });
+    }
+  }
+  Layanan_form_num.init({
+    userinfo_id: DataTypes.INTEGER,
+    // no_request: DataTypes.STRING,
+    layanan_id: DataTypes.INTEGER,
+    fileoutput: DataTypes.STRING,
+    pesan: DataTypes.STRING,
+    tgl_selesai: DataTypes.DATEONLY,
+    isonline: DataTypes.INTEGER,
+    status: DataTypes.SMALLINT,
+    updated_by: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Layanan_form_num',
+  });
+  return Layanan_form_num;
+};
