b266a6dd68caa8fb4c8f6886caf043dc33295279
My Name <lulu.agustin105319@students.unila.ac.id>
2024-09-03 17:43:11 +0700

    add new route

--- routes/api.route.js
+++ routes/api.route.js
@@ -6,6 +6,8 @@ const bidangRoute = require('./bidang.route');
 const layananRoute = require('./layanan.route');
 const layananformRoute = require('./layananform.route');
 const layananfileRoute = require('./layananfile.route');
+const inputformRoute = require('./inputform.route');
+const historyformRoute = require('./historyform.route');
 const layanansuratRoute = require('./layanansurat.route');
 const kecamatanRoute = require('./kecamatan.route');
 const desaRoute = require('./desa.route');
@@ -21,6 +23,8 @@ module.exports = function (app, urlApi) {
     app.use(urlApi, layananRoute);
     app.use(urlApi, layananformRoute);
     app.use(urlApi, layananfileRoute);
+    app.use(urlApi, inputformRoute);
+    app.use(urlApi, historyformRoute);
     app.use(urlApi, layanansuratRoute);
     app.use(urlApi, kecamatanRoute);
     app.use(urlApi, desaRoute);
