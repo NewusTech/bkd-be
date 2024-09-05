9518a35550f3dd254ae2b074d6ffdc1dd24cf846
My Name <lulu.agustin105319@students.unila.ac.id>
2024-09-05 16:24:51 +0700

    add route termcondition

---
+++ routes/termcondition.route.js
@@ -0,0 +1,16 @@
+const termconditionController = require('../controllers/termcondition.controller');
+
+const mid = require('../middlewares/auth.middleware');
+
+const express = require('express');
+const route = express.Router();
+
+const multer = require('multer');
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+ 
+route.get('/user/term-condition/get', termconditionController.getTermCondition); 
+route.put('/user/term-condition/update', [mid.checkRolesAndLogout(['Super Admin'])], upload.fields([{ name: 'desc', maxCount: 1 }, { name: 'privacy', maxCount: 1 }]), termconditionController.updatetermcond); 
+
+module.exports = route;
