bdbf6b5498d1fc3d5476ca1f7e3c725577c1aca1
My Name <lulu.agustin105319@students.unila.ac.id>
2024-09-09 12:06:07 +0700

    add galeri route

---
+++ routes/galeri.route.js
@@ -0,0 +1,19 @@
+const GaleriController = require('../controllers/galeri.controller');
+
+const mid = require('../middlewares/auth.middleware');
+
+const express = require('express');
+const route = express.Router();
+
+const multer = require('multer');
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+route.post('/user/galeri/create', [mid.checkRolesAndLogout(['Super Admin'])], upload.single('image'), GaleriController.createGaleri);
+route.get('/user/galeri/get', GaleriController.getGaleri); 
+route.get('/user/galeri/get/:slug', GaleriController.getGaleriBySlug); 
+route.put('/user/galeri/update/:slug', [mid.checkRolesAndLogout(['Super Admin'])], upload.single('image'), GaleriController.updateGaleri); 
+route.delete('/user/galeri/delete/:slug', [mid.checkRolesAndLogout(['Super Admin'])], GaleriController.deleteGaleri);
+
+module.exports = route;
