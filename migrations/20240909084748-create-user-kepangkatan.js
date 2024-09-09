3d91f1b2012274d2bd00893f231bd92db9751fed
My Name <lulu.agustin105319@students.unila.ac.id>
2024-09-09 16:03:13 +0700

    new migration kepangkatan

---
+++ migrations/20240909084748-create-user-kepangkatan.js
@@ -0,0 +1,44 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('User_kepangkatans', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      jenjang_kepangkatan: {
+        type: Sequelize.STRING
+      },
+      tmt: {
+        type: Sequelize.DATE,
+        unique: true
+      },
+      no_sk_pangkat: {
+        type: Sequelize.STRING,
+        unique: true
+      },
+      tgl_sk_pangkat: {
+        type: Sequelize.DATE,
+        unique: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        type: Sequelize.DATE
+      }
+    });
+
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('User_kepangkatans');
+  }
+};
