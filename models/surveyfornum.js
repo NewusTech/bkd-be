f69c83490e37d5b657c8c50f47da690f82c968db
My Name <lulu.agustin105319@students.unila.ac.id>
2024-09-10 15:54:03 +0700

    create the model

---
+++ models/surveyfornum.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Survey_form_num extends Model {
+    static associate(models) {
+      Survey_form_num.belongsTo(models.Layanan, {
+        foreignKey: 'layanan_id',
+      });
+      Survey_form_num.hasMany(models.Survey_form_input, {
+        foreignKey: 'surveyformnum_id',
+      });
+      Survey_form_num.belongsTo(models.User_info, {
+        foreignKey: 'userinfo_id',
+      });
+    }
+  }
+  Survey_form_num.init({
+    no_skm: DataTypes.STRING,
+    userinfo_id: DataTypes.INTEGER,
+    layanan_id: DataTypes.INTEGER,
+    date: DataTypes.STRING,
+    feedback: DataTypes.TEXT,
+    sequelize,
+    modelName: 'Survey_form_num',
+  });
+  return Survey_form_num;
+};
