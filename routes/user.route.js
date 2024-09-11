afb6dbdc22227221cdd27c243387d7445a71931f
My Name <lulu.agustin105319@students.unila.ac.id>
2024-09-11 13:36:23 +0700

    edit route user

--- routes/user.route.js
+++ routes/user.route.js
@@ -10,12 +10,12 @@ route.post('/login', userController.loginUser);
 route.post('/logout', [mid.checkRolesAndLogout(['Super Admin', 'User'])], userController.logoutUser); 
 
 // API UNTUK ADMIN / SUPER ADMIN
-route.get('/user/get', [mid.checkRolesAndLogout(['Super Admin'])], userController.getuser); 
-route.get('/user/get/:slug', [mid.checkRolesAndLogout(['Super Admin'])], userController.getuserByslug); 
-route.delete('/user/delete/:slug', [mid.checkRolesAndLogout(['Super Admin'])], userController.deleteuser);
+route.get('/user/get', [mid.checkRolesAndLogout(['Super Admin'])], userController.getUser); 
+route.get('/user/get/:slug', [mid.checkRolesAndLogout(['Super Admin'])], userController.getUserBySlug); 
+route.delete('/user/delete/:slug', [mid.checkRolesAndLogout(['Super Admin'])], userController.deleteUser);
 
 //API BUAT USER
-route.get('/getforuser', [mid.checkRolesAndLogout(['User', 'Super Admin'])], userController.getforuser); 
+route.get('/getforuser', [mid.checkRolesAndLogout(['User', 'Super Admin'])], userController.getForUser); 
 
 route.post('/changepassword/:slug', [mid.checkRolesAndLogout(['Admin Verifikasi', 'Admin Layanan', 'Super Admin', 'User'])], userController.changePassword); 
 
