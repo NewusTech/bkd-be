948056c2ff8899c3c7861a9beb8545cbc210aa31
My Name <lulu.agustin105319@students.unila.ac.id>
2024-09-14 14:59:56 +0700

    tambah relasi dengan user

--- models/user.js
+++ models/user.js
@@ -32,7 +32,9 @@ module.exports = (sequelize, DataTypes) => {
       User.belongsTo(models.User_pendidikan, {
         foreignKey: 'userpendidikan_id',
       });
-
+      User.belongsTo(models.User_dokumen, {
+        foreignKey: 'userdokumen_id',
+      });
       User.belongsToMany(models.Permission, {
         through: 'User_permissions',
         as: 'permissions',
@@ -50,6 +52,7 @@ module.exports = (sequelize, DataTypes) => {
     userkepangkatan_id: DataTypes.INTEGER,
     userpendidikan_id: DataTypes.INTEGER,
     userjabatan_id: DataTypes.INTEGER,
+    userdokumen_id: DataTypes.INTEGER,
     deletedAt: DataTypes.DATE,
     resetpasswordtoken: DataTypes.STRING,
     resetpasswordexpires: DataTypes.DATE,
