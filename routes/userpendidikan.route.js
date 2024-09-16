44179ac675230f42ac2351885ac08c0786f2e833
My Name <lulu.agustin105319@students.unila.ac.id>
2024-09-16 20:57:41 +0700

    create user pendidikan route

--- routes/userpendidikan.route.js
+++ routes/userpendidikan.route.js
@@ -1,21 +1,15 @@
-const userinfoController = require('../controllers/userinfo.controller');
+const userpendidikanController = require('../controllers/userpendidikan.controller');
 
 const mid = require('../middlewares/auth.middleware');
 
 const express = require('express');
 const route = express.Router();
 
-const multer = require('multer');
+route.get('/user/pendidikan/get', [mid.checkRolesAndLogout(['Super Admin', 'User'])], userpendidikanController.getUserDataPendidikan); 
+route.get('/user/pendidikan/get/:slug', [mid.checkRolesAndLogout(['Super Admin', 'User'])], userpendidikanController.getUserPendidikanByID); 
+route.delete('/user/pendidikan/delete/:slug', [mid.checkRolesAndLogout(['Super Admin'])], userpendidikanController.deleteUserPendidikan);
 
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
-
-route.get('/user/pendidikan/get', [mid.checkRolesAndLogout(['Super Admin', 'User'])], userinfoController.getUserData); 
-route.get('/user/pendidikan/get/:id', [mid.checkRolesAndLogout(['Super Admin', 'User'])], userinfoController.getUserBySlug); 
-route.delete('/user/pendidikan/delete/:id', [mid.checkRolesAndLogout(['Super Admin'])], userinfoController.deleteUser);
-
-route.post('/user/pendidikan/create', [mid.checkRolesAndLogout(['Super Admin'])], upload.fields([ ]), userinfoController.createUserInfo); 
-route.put('/user/pendidikan/update/:id', [mid.checkRolesAndLogout(['Super Admin', 'User'])], upload.single('image_profile'), userinfoController.updateUserInfo);
-route.put('/user/pendidikan/updatedocs/:id', [mid.checkRolesAndLogout(['Super Admin', 'User'])], upload.fields([ ]), userinfoController.updateUserDocs);
+route.post('/user/pendidikan/create', [mid.checkRolesAndLogout(['Super Admin', 'User'])], userpendidikanController.createUserPendidikan); 
+route.put('/user/pendidikan/update/:slug', [mid.checkRolesAndLogout(['Super Admin', 'User'])], userpendidikanController.updateUserPendidikan);
 
 module.exports = route;
