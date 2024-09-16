d15c1135e83d841f6af643d3c9a828399c58946f
My Name <lulu.agustin105319@students.unila.ac.id>
2024-09-16 20:57:51 +0700

    create user jabatan route

--- routes/userjabatan.route.js
+++ routes/userjabatan.route.js
@@ -13,6 +13,6 @@ const upload = multer({ storage: storage });
 route.get('/user/jabatan/get', [mid.checkRolesAndLogout(['Super Admin', 'User'])], userjabatanController.getUserDataJabatan); 
 route.get('/user/jabatan/get/:slug', [mid.checkRolesAndLogout(['Super Admin', 'User'])], userjabatanController.getUserJabatanByID); 
 route.delete('/user/jabatan/delete/:slug', [mid.checkRolesAndLogout(['Super Admin'])], userjabatanController.deleteUserJabatan);
-
+route.post('/user/jabatan/create', [mid.checkRolesAndLogout(['Super Admin', 'User'])], userjabatanController.createUserJabatan); 
 
 module.exports = route;
