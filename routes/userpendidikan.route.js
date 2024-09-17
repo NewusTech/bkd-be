6f0934a6a0ba82cbeff9df15137df757b8852133
My Name <lulu.agustin105319@students.unila.ac.id>
2024-09-17 17:01:17 +0700

    update user pendidikan

--- routes/userpendidikan.route.js
+++ routes/userpendidikan.route.js
@@ -10,6 +10,6 @@ route.get('/user/pendidikan/get/:slug', [mid.checkRolesAndLogout(['Super Admin',
 route.delete('/user/pendidikan/delete/:slug', [mid.checkRolesAndLogout(['Super Admin'])], userpendidikanController.deleteUserPendidikan);
 
 route.post('/user/pendidikan/create', [mid.checkRolesAndLogout(['Super Admin', 'User'])], userpendidikanController.createUserPendidikan); 
-route.put('/user/pendidikan/update/:slug', [mid.checkRolesAndLogout(['Super Admin', 'User'])], userpendidikanController.updateUserPendidikan);
+route.post('/user/pendidikan/update', [mid.checkRolesAndLogout(['Super Admin', 'User'])], userpendidikanController.updateUserPendidikan);
 
 module.exports = route;
