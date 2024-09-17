2b05c44b4180e3d201f897cdb55b967e246a2947
My Name <lulu.agustin105319@students.unila.ac.id>
2024-09-17 15:44:39 +0700

    add api struktur organisasi

--- routes/api.route.js
+++ routes/api.route.js
@@ -21,6 +21,7 @@ const faqRoute = require('./faq.route');
 const surveyformRoute = require('./surveyform.route');
 const bannerRoute = require('./banner.route');
 const bkdprofileRoute = require('./bkdprofile.route');
+const bkdstrukturRoute = require('./bkdstruktur.route');
 const beritaRoute = require('./berita.route');
 
 module.exports = function (app, urlApi) {
@@ -47,5 +48,6 @@ module.exports = function (app, urlApi) {
     app.use(urlApi, surveyformRoute);
     app.use(urlApi, bannerRoute);
     app.use(urlApi, bkdprofileRoute);
+    app.use(urlApi, bkdstrukturRoute);
     app.use(urlApi, beritaRoute);
 }
