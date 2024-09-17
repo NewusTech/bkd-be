73690048fbd7c493fc5d03a215b98dafef577645
My Name <lulu.agustin105319@students.unila.ac.id>
2024-09-17 15:05:19 +0700

    create route berita

---
+++ routes/berita.route.js
@@ -0,0 +1,19 @@
+const beritaController = require('../controllers/berita.controller');
+
+const mid = require('../middlewares/auth.middleware');
+
+const express = require('express');
+const route = express.Router();
+
+const multer = require('multer');
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+route.post('/user/berita/create', [mid.checkRolesAndLogout(['Super Admin'])], upload.single('image'), beritaController.createBerita);
+route.get('/user/berita/get', beritaController.getBerita); 
+route.get('/user/berita/get/:slug', beritaController.getBeritaBySlug); 
+route.put('/user/berita/update/:slug', [mid.checkRolesAndLogout(['Super Admin'])], upload.single('image'), beritaController.updateBerita); 
+route.delete('/user/berita/delete/:slug', [mid.checkRolesAndLogout(['Super Admin'])], beritaController.deleteBerita);
+
+module.exports = route;
