c40b089c6d0a3fd4d2949cb8931e4446c81f5087
My Name <lulu.agustin105319@students.unila.ac.id>
2024-09-18 16:36:02 +0700

    create migration user penghargaan

---
+++ migrations/20240918085124-create-user-penghargaan.js
@@ -0,0 +1,41 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('User_penghargaans', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+      },
+      uraian_penghargaan: {
+        type: Sequelize.STRING
+      },
+      tanggal_penghargaan: {
+        type: Sequelize.DATE,
+      },
+      instansi_penghargaan: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        type: Sequelize.DATE
+      }
+    });
+
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('User_penghargaans');
+  }
+};
