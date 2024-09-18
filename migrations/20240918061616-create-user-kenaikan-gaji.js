66e3cde1fb2b361b73baeea5a996425ddc10f76f
My Name <lulu.agustin105319@students.unila.ac.id>
2024-09-18 15:00:18 +0700

    create migration user kgb

---
+++ migrations/20240918061616-create-user-kenaikan-gaji.js
@@ -0,0 +1,44 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('User_KGBS', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+      },
+      uraian_berkala: {
+        type: Sequelize.STRING
+      },
+      tmt: {
+        type: Sequelize.DATE,
+      },
+      no_sk_pangkat: {
+        type: Sequelize.STRING,
+      },
+      tgl_sk_pangkat: {
+        type: Sequelize.DATE,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        type: Sequelize.DATE
+      }
+    });
+
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('User_KGBS');
+  }
+};
