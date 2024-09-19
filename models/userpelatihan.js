cce50109cafe6d2789f3d4af0406e4ba4c8e4285
My Name <lulu.agustin105319@students.unila.ac.id>
2024-09-19 10:44:43 +0700

    add model user pelatihan

---
+++ models/userpelatihan.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User_pelatihan extends Model {
+    static associate(models) {
+      User_pelatihan.hasOne(models.User, {
+        foreignKey: 'userpelatihan_id',
+      });
+    }
+  }
+  User_pelatihan.init({
+    user_id: DataTypes.INTEGER,
+    uraian_pelatihan: DataTypes.STRING,
+    lama_pelatihan: DataTypes.STRING,
+    no_surat_pelatihan: DataTypes.STRING,
+    tanggal_pelatihan: DataTypes.STRING,
+    tempat_pelatihan: DataTypes.STRING,
+    deletedAt: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'User_pelatihan',
+  });
+  return User_pelatihan;
+};
