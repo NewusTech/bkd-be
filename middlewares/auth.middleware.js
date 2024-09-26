45f063bf43cd14c5e8b4c4fb770a594410a6fd0c
My Name <lulu.agustin105319@students.unila.ac.id>
2024-09-26 15:37:36 +0700

    change middleware

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -6,59 +6,53 @@ const jwt = require('jsonwebtoken');
 const checkRolesAndLogout = (allowedRoles) => async (req, res, next) => {
     let token;
     try {
-        if (req.headers.authorization && req.headers.authorization.split(' ')[1]) {
-            token = req.headers.authorization.split(' ')[1];
-        } else {
-            throw new Error('Token not found');
-        }
+        token = req.headers.authorization.split(' ')[1];
     } catch (err) {
-        return res.status(403).json(response(403, 'Unauthorized: invalid or missing token'));
+        res.status(403).json(response(403, 'Unauthorized: invalid or missing token'));
+        return;
+    }
+
+    if (!token) {
+        res.status(403).json(response(403, 'Unauthorized: token not found'));
+        return;
     }
 
     jwt.verify(token, baseConfig.auth_secret, async (err, decoded) => {
         if (err) {
-            return res.status(403).json(response(403, 'Unauthorized: token expired or invalid'));
+            res.status(403).json(response(403, 'Unauthorized: token expired or invalid'));
+            return;
         }
 
-        req.user = decoded; // Menyimpan data user di req
+        req.user = decoded;
+
         const tokenCheck = await Token.findOne({ where: { token } });
 
         if (tokenCheck) {
-            return res.status(403).json(response(403, 'Unauthorized: already logged out'));
+            res.status(403).json(response(403, 'Unauthorized: already logged out'));
+            return;
         }
 
         if (allowedRoles.includes(req.user.role)) {
             next();
         } else {
-            return res.status(403).json(response(403, 'Forbidden: insufficient access rights'));
+            res.status(403).json(response(403, 'Forbidden: insufficient access rights'));
         }
     });
 };
 
-
 const checkRoles = () => async (req, res, next) => {
     let token;
     try {
-        if (req.headers.authorization && req.headers.authorization.split(' ')[1]) {
-            token = req.headers.authorization.split(' ')[1];
-        } else {
-            throw new Error('Token not found');
-        }
+        token = req.headers.authorization.split(' ')[1];
     } catch (err) {
-        return res.status(403).json(response(403, 'Unauthorized: invalid or missing token'));
     }
 
-    jwt.verify(token, baseConfig.auth_secret, (err, decoded) => {
-        if (err) {
-            return res.status(403).json(response(403, 'Unauthorized: token expired or invalid'));
-        }
-
-        req.user = decoded; // Menyimpan data user di req untuk digunakan di route handler
+    jwt.verify(token, baseConfig.auth_secret, async (err, decoded) => {
+        data = decoded;
         next();
     });
 };
 
 module.exports = {
-    checkRolesAndLogout,
-    checkRoles
+    checkRolesAndLogout, checkRoles
 };
