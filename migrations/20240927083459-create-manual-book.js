9da990b214c71ec95ddedfa9fa0cfae219083ee3
My Name <lulu.agustin105319@students.unila.ac.id>
2024-09-27 15:56:56 +0700

    new migration manual book

---
+++ migrations/20240927083459-create-manual-book.js
@@ -0,0 +1,35 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Manual_books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      dokumen: {
+        type: Sequelize.STRING
+      },
+      role_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Manual_books');
+  }
+};
