8ddd6d1a79d3ba9391d9990fdcaec9a57e5f7a7c
My Name <lulu.agustin105319@students.unila.ac.id>
2024-09-27 15:57:19 +0700

    create API manual book

---
+++ models/manualbook.js
@@ -0,0 +1,22 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Manual_book extends Model {
+    static associate(models) {
+      Manual_book.belongsTo(models.Role, {
+        foreignKey: 'role_id'
+      });
+    }
+  }
+  Manual_book.init({
+    title: DataTypes.STRING,
+    dokumen: DataTypes.STRING,
+    role_id: DataTypes.INTEGER,
+  }, {
+    sequelize,
+    modelName: 'Manual_book',
+  });
+  return Manual_book;
+};
---
+++ routes/manualbook.route.js
@@ -0,0 +1,17 @@
+const manualbookController = require('../controllers/manualbook.controller');
+
+const mid = require('../middlewares/auth.middleware');
+
+const express = require('express');
+const route = express.Router();
+
+const multer = require('multer');
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+ 
+route.get('/user/manual/book/get', manualbookController.getManualBook);
+route.get('/user/manual/book/get/:id', manualbookController.getManualBookById); 
+route.put('/user/manual/book/update/:id', [mid.checkRolesAndLogout(['Super Admin'])], upload.fields([{ name: 'manualbook', maxCount: 1 }]), manualbookController.updateManualBook);
+
+module.exports = route;
