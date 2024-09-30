3a1aee5c0fbff2dad3f2c47c8ad118bed35b9c87
My Name <lulu.agustin105319@students.unila.ac.id>
2024-09-30 16:51:35 +0700

    create migration index kepuasaan

---
+++ migrations/20240930083130-create-index-kepuasaan.js
@@ -0,0 +1,48 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('User_feedbacks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userinfo_id: {
+        type: Sequelize.INTEGER
+      },
+      layanan_id: {
+        type: Sequelize.INTEGER
+      },
+      question_1: {
+        type: Sequelize.INTEGER
+      },
+      question_2: {
+        type: Sequelize.INTEGER
+      },
+      question_3: {
+        type: Sequelize.INTEGER
+      },
+      question_4: {
+        type: Sequelize.INTEGER
+      },
+      feedback: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  //untuk drop table ketika melakukan revert migrations
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('User_feedbacks');
+  }
+};
