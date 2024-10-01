bdb336787134316c109763e17100229f281abcc6
My Name <lulu.agustin105319@students.unila.ac.id>
2024-10-01 11:05:31 +0700

    add route

---
+++ routes/regulasi.route.js
@@ -0,0 +1,19 @@
+const RegulasiController = require('../controllers/regulasi.controller');
+
+const mid = require('../middlewares/auth.middleware');
+
+const express = require('express');
+const route = express.Router();
+
+const multer = require('multer');
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+route.post('/user/regulasi/create', [mid.checkRolesAndLogout(['Super Admin'])], upload.single('file'), RegulasiController.createRegulasi);
+route.get('/user/regulasi/get', RegulasiController.getRegulasi); 
+route.get('/user/regulasi/get/:id', RegulasiController.getRegulasiByID); 
+route.put('/user/regulasi/update/:id', [mid.checkRolesAndLogout(['Super Admin'])], upload.single('file'), RegulasiController.updateRegulasi); 
+route.delete('/user/regulasi/delete/:id', [mid.checkRolesAndLogout(['Super Admin'])], RegulasiController.deleteRegulasi);
+
+module.exports = route;
