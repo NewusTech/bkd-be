1929cc5b2365cc85357a9a4fa60ddaecdbdfb9d3
My Name <lulu.agustin105319@students.unila.ac.id>
2024-10-02 14:23:32 +0700

    change route input form

--- routes/inputform.route.js
+++ routes/inputform.route.js
@@ -14,8 +14,8 @@ route.post('/user/input/form/create/:idlayanan', [mid.checkRolesAndLogout(['User
 route.put('/user/input/form/update/:idlayanannum', [mid.checkRolesAndLogout(['User', 'Super Admin'])], upload.any(), layananforminput.updateData);
 route.get('/user/input/form/detail/:idlayanannum', [mid.checkRolesAndLogout(['User', 'Super Admin'])], layananforminput.getDetailInputForm);
 
-route.put('/user/input/form/updatestatus/:idlayanannum', [mid.checkRolesAndLogout(['Admin Instansi', 'Admin Verifikasi', 'Admin Layanan', 'Super Admin'])], upload.any(), layananforminput.updateStatusPengajuan);
+route.put('/user/input/form/updatestatus/:idlayanannum', [mid.checkRolesAndLogout(['Super Admin'])], upload.any(), layananforminput.updateStatusPengajuan);
 
-route.put('/user/input/form/file/:idlayanannum', [mid.checkRolesAndLogout(['Admin Instansi', 'Admin Verifikasi', 'Admin Layanan', 'Super Admin'])], upload.fields([{ name: 'file', maxCount: 1 }, { name: 'sertif', maxCount: 1 }]), layananforminput.uploadFileHasil);
+route.put('/user/input/form/file/:idlayanannum', [mid.checkRolesAndLogout(['Super Admin'])], upload.fields([{ name: 'file', maxCount: 1 }, { name: 'sertif', maxCount: 1 }]), layananforminput.uploadFileHasil);
 
 module.exports = route;
