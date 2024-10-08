187da491b020f01e475e540b7116514b6b810c62
My Name <lulu.agustin105319@students.unila.ac.id>
2024-10-08 14:12:20 +0700

    add route nilai kepuasaan

--- routes/userfeedback.route.js
+++ routes/userfeedback.route.js
@@ -6,10 +6,10 @@ const express = require('express');
 const route = express.Router();
 
 route.post('/user/feedback/create/:idlayanan', [mid.checkRolesAndLogout(['User'])], userfeedbackController.createFeedback);
-
-route.get('/user/history/feedback', [mid.checkRolesAndLogout(['Super Admin', 'User'])], userfeedbackController.getHistoryByBidang);
-
 route.get('/user/get/history/feedback', [mid.checkRolesAndLogout(['User'])], userfeedbackController.getHistoryForUser);
 route.get('/user/history/feedback/detail/:idfeedback', [mid.checkRolesAndLogout(['Super Admin', 'User'])], userfeedbackController.getDetailHistoryFeedback);
+
+route.get('/user/history/feedback', [mid.checkRolesAndLogout(['Super Admin'])], userfeedbackController.getHistoryByBidang);
+route.get('/user/history/feedback/:idlayanan', [mid.checkRolesAndLogout(['Super Admin'])], userfeedbackController.getHistoryByLayanan);
 
 module.exports = route;
