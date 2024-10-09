51af4e6ba8659379376d3283e38e3c982804be20
My Name <lulu.agustin105319@students.unila.ac.id>
2024-10-09 12:04:29 +0700

    update hak akses untuk history pengajuan

--- routes/historyform.route.js
+++ routes/historyform.route.js
@@ -5,11 +5,11 @@ const mid = require('../middlewares/auth.middleware');
 const express = require('express');
 const route = express.Router();
 
-route.get('/user/history/form', [mid.checkRolesAndLogout(['Super Admin', 'User'])], layananforminput.getHistoryFormUser);
+route.get('/user/history/form', [mid.checkRolesAndLogout(['Super Admin', 'User', 'Admin Verifikasi', 'Kepala Bidang', 'Sekretaris Dinas', 'Kepala Dinas'])], layananforminput.getHistoryFormUser);
 
 route.get('/user/history/dokumen', [mid.checkRolesAndLogout(['Super Admin', 'User'])], layananforminput.getHistoryDokumen);
 
-route.get('/user/history/form/:idforminput', [mid.checkRolesAndLogout([ 'Super Admin', 'User'])], layananforminput.getHistoryById);
+route.get('/user/history/form/:idforminput', [mid.checkRolesAndLogout([ 'Super Admin', 'User', 'Admin Verifikasi', 'Kepala Bidang', 'Sekretaris Dinas', 'Kepala Dinas'])], layananforminput.getHistoryById);
 
 route.get('/historyform/pdf', [mid.checkRolesAndLogout(['Super Admin'])], layananforminput.pdfHistoryFormUser);
 
