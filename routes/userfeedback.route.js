ca8309f305a007ff7bb054661f6304e8cb163380
My Name <lulu.agustin105319@students.unila.ac.id>
2024-10-11 17:05:44 +0700

    change permissions

--- routes/userfeedback.route.js
+++ routes/userfeedback.route.js
@@ -9,7 +9,8 @@ route.post('/user/feedback/create/:idlayanan', [mid.checkRolesAndLogout(['User']
 route.get('/user/get/history/feedback', [mid.checkRolesAndLogout(['User'])], userfeedbackController.getHistoryForUser);
 route.get('/user/history/feedback/detail/:idfeedback', [mid.checkRolesAndLogout(['Super Admin', 'User'])], userfeedbackController.getDetailHistoryFeedback);
 
-route.get('/user/history/feedback', [mid.checkRolesAndLogout(['Super Admin'])], userfeedbackController.getHistoryByBidang);
-route.get('/user/history/feedback/:idlayanan', [mid.checkRolesAndLogout(['Super Admin'])], userfeedbackController.getHistoryByLayanan);
+route.get('/user/history/feedback', [mid.checkRolesAndLogout(['Super Admin', 'Admin Verifikasi', 'Kepala Bidang', 'Sekretaris Dinas', 'Kepala Dinas', 'Sekretaris Daerah'])], userfeedbackController.getHistoryByBidang);
+route.get('/user/history/feedback/:idlayanan', [mid.checkRolesAndLogout(['Super Admin', 'Admin Verifikasi', 'Kepala Bidang', 'Sekretaris Dinas', 'Kepala Dinas', 'Sekretaris Daerah'])], userfeedbackController.getHistoryByLayanan);
 
+route.get('/user/history/feedback/get/pdf', [mid.checkRolesAndLogout(['Admin Instansi', 'Admin Verifikasi', 'Admin Layanan', 'Super Admin'])], userfeedbackController.getPDFHistoryByBidang);
 module.exports = route;
