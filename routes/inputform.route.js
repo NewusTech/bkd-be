df057977eddb98a9722ea553a6dbf7acc076675e
My Name <lulu.agustin105319@students.unila.ac.id>
2024-10-16 15:24:01 +0700

    add permissions

--- routes/inputform.route.js
+++ routes/inputform.route.js
@@ -15,7 +15,7 @@ route.put('/user/input/form/update/:idlayanannum', [mid.checkRolesAndLogout(['Us
 route.get('/user/input/form/detail/:idlayanannum', [mid.checkRolesAndLogout(['User', 'Super Admin', 'Admin Verifikasi', 'Kepala Bidang', 'Sekretaris Dinas', 'Kepala Dinas', 'Sekretaris Daerah'])], layananforminput.getDetailInputForm);
 
 route.put('/user/input/form/updatestatus/:idlayanannum', [mid.checkRolesAndLogout(['Super Admin', 'Admin Verifikasi', 'Kepala Bidang', 'Sekretaris Dinas', 'Kepala Dinas', 'Sekretaris Daerah'])], upload.any(), layananforminput.updateStatusPengajuan);
-route.put('/user/form/:idlayanannum/signing', [mid.checkRolesAndLogout(['Super Admin'])], upload.single('sign'), layananforminput.uploadSign);
+route.put('/user/form/:idlayanannum/signing', [mid.checkRolesAndLogout(['Super Admin', 'Sekretaris Dinas', 'Kepala Dinas', 'Sekretaris Daerah' ])], upload.single('sign'), layananforminput.uploadSign);
 route.put('/user/input/form/file/:idlayanannum', [mid.checkRolesAndLogout(['Super Admin'])], upload.fields([{ name: 'file', maxCount: 1 }]), layananforminput.uploadFileHasil);
 
 module.exports = route;
