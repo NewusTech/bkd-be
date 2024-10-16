6ef2e7404536071e297595a80e27f4a8790e7a61
My Name <lulu.agustin105319@students.unila.ac.id>
2024-10-16 15:11:40 +0700

    change permissions

--- routes/inputform.route.js
+++ routes/inputform.route.js
@@ -14,7 +14,7 @@ route.post('/user/input/form/create/:idlayanan', [mid.checkRolesAndLogout(['User
 route.put('/user/input/form/update/:idlayanannum', [mid.checkRolesAndLogout(['User', 'Super Admin'])], upload.any(), layananforminput.updateDataForm);
 route.get('/user/input/form/detail/:idlayanannum', [mid.checkRolesAndLogout(['User', 'Super Admin', 'Admin Verifikasi', 'Kepala Bidang', 'Sekretaris Dinas', 'Kepala Dinas', 'Sekretaris Daerah'])], layananforminput.getDetailInputForm);
 
-route.put('/user/input/form/updatestatus/:idlayanannum', [mid.checkRolesAndLogout(['Super Admin'])], upload.any(), layananforminput.updateStatusPengajuan);
+route.put('/user/input/form/updatestatus/:idlayanannum', [mid.checkRolesAndLogout(['Super Admin', 'Admin Verifikasi', 'Kepala Bidang', 'Sekretaris Dinas', 'Kepala Dinas', 'Sekretaris Daerah'])], upload.any(), layananforminput.updateStatusPengajuan);
 route.put('/user/form/:idlayanannum/signing', [mid.checkRolesAndLogout(['Super Admin'])], upload.single('sign'), layananforminput.uploadSign);
 route.put('/user/input/form/file/:idlayanannum', [mid.checkRolesAndLogout(['Super Admin'])], upload.fields([{ name: 'file', maxCount: 1 }]), layananforminput.uploadFileHasil);
 
